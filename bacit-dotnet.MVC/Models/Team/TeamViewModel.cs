21835dd0bfc6da76cba7cc947487f17f45ee36cd
Linh Phuong Tra <phuonglinhtra92@gmail.com>
2022-09-22 10:25:32 +0200

    Add TeamViewModel

---
+++ bacit-dotnet.MVC/Models/Team/TeamViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bacit_dotnet.MVC.Models.Team
+{
+    public class TeamViewModel
+    {
+        public int TeamId { get; set; } 
+        public string TeamName { get; set; } = default!;
+
+        //Employee
+        [Required]
+        public string Name { get; set; } = default!;
+        
+    }
+}
